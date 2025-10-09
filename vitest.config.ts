42bd529f8b6dfb8b873a4451d8001ba784fcb08c
Artyom Konukhov <artemkonuhov008@gmail.com>
2025-10-09 15:43:12 +0300

    chore: fix vitest config for esm

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,25 +1,28 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
-    css: true,
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      exclude: [
-        'node_modules/',
-        'src/test/',
-        '**/*.d.ts',
-        '**/*.config.*',
-        'vite.config.ts',
-        'vitest.config.ts',
-      ],
+export default defineConfig(async () => {
+  const { default: react } = await import('@vitejs/plugin-react')
+
+  return {
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./src/test/setup.ts'],
+      css: true,
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html'],
+        exclude: [
+          'node_modules/',
+          'src/test/',
+          '**/*.d.ts',
+          '**/*.config.*',
+          'vite.config.ts',
+          'vitest.config.ts',
+        ],
+      },
     },
-  },
+  }
 })
